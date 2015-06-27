841d49bbf5a9fbedae268dac2a267125210bd10c
Aneury Casado <aneury.casado@gmail.com>
2015-06-27 19:42:14 -0400

    Only one thing left. Hotter and colder screens

--- js/game.js
+++ js/game.js
@@ -2,14 +2,17 @@ $(function(){
   var randomNumber = pickRandomNumber(1,100)
   console.log(randomNumber)
   var numberOfGuesses = 5
+  var guessesSoFar = [[],[]]
   $("#submit").click(function(){
     numberOfGuesses-=1
     updateNumberOfGuesses(numberOfGuesses)
     var guess = parseInt($("#guess").val())
     if(gameOver(numberOfGuesses,randomNumber,guess)){
+      numberOfGuesses = updateGuessesSoFar(guess,'Loser',guessesSoFar)
       loser()
     }else{
         if(isNaN(guess)){
+          guessesSoFar = updateGuessesSoFar(guess,"IM",guessesSoFar)
           if(numberOfGuesses == 4){
             okayFirstTime()
           }else if(numberOfGuesses == 3){
@@ -19,10 +22,13 @@ $(function(){
           }else if(numberOfGuesses == 1){
             yourGoingToLose()
           }
+        }else if(guessesSoFar[0].indexOf(guess) > -1){
+          console.log("Here")
+          guessesSoFar = updateGuessesSoFar(guess,'R',guessesSoFar)
+          dontRepeatYourself()
         }else{
           var difference = randomNumber - guess
           var direction = ""
-          console.log(difference == 0)
           if(difference > 0){
             direction = "Higher"
           }else{
@@ -30,12 +36,16 @@ $(function(){
           }
           difference = Math.abs(difference)
           if(difference > 40){
+            guessesSoFar = updateGuessesSoFar(guess,'SC',guessesSoFar)
             superCold(direction)
-          }else if(difference > 20 && difference < 40){
+          }else if(difference > 20 && difference <= 40){
+            guessesSoFar = updateGuessesSoFar(guess,'C',guessesSoFar)
             cold(direction)
-          }else if(difference < 20 && difference > 10){
+          }else if(difference <= 20 && difference > 10){
+            guessesSoFar = updateGuessesSoFar(guess,'W',guessesSoFar)
             warm(direction)
-          }else if(difference < 10 && difference != 0){
+          }else if(difference <= 10 && difference != 0){
+            guessesSoFar = updateGuessesSoFar(guess,'H',guessesSoFar)
             hot(direction)
           }else if(difference === 0){
             winner()
@@ -50,7 +60,43 @@ $(function(){
       var body = "<h1>" + randomNumber.toString() + "</h2>"
       setModalWindow(header,body)
     })
+    $(document).keypress(function(event){
+      if(event.which == 13){
+        $('#submit').trigger("click");
+      }
+    });
 })
+
+function updateGuessesSoFar(guess,temp,guessesSoFar){
+  guessesSoFar[0].push(guess)
+  guessesSoFar[1].push(temp)
+  updateGuessesSoFarHTML(guessesSoFar)
+  return guessesSoFar
+}
+
+function dontRepeatYourself(){
+  $("#talk").html("Come on. Don't repeat yourself.")
+  $("#indicator").html('<span class="label label-default">Repeat</span></h1>')
+  var header = "<h2>You already guessed that number.</h2>"
+  var body = '<img src="img/repeat.png" alt="Repeat">'
+  setModalWindow(header,body)
+}
+
+function updateGuessesSoFarHTML(guessesSoFar){
+  var string = createString(guessesSoFar)
+  $("#guessesSoFar").html('<span class="label label-info">' + string + '</span></h1>')
+}
+
+function createString(guessesSoFar){
+  var string = ""
+  for(var i = 0; i < guessesSoFar[0].length;i++){
+    var guess = guessesSoFar[0][i]
+    var temp = guessesSoFar[1][i]
+    string = string + guess.toString() + " " + temp + " | "
+  }
+  //string = substring(0,string.length-1)
+  return string
+}
 
 function pickRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -114,7 +160,7 @@ function yourGoingToLose(){
 function superCold(direction){
   $("#talk").html("We have found the cure to global warming people.")
   $("#indicator").html('<span class="label label-primary">Super Cold. Guess ' + direction + '</span></h1>')
-  var header = "<h2>You're colder than this place!</h2>"
+  var header = "<h2>You have real problems.</h2>"
   var body = '<img src="img/supercold.png" alt="Super Cold">'
   setModalWindow(header,body)
 }
@@ -139,7 +185,7 @@ function hot(direction){
   $("#talk").html("Maybe I should have you play the lotto for me.")
   $("#indicator").html('<span class="label label-danger">Hot. Guess ' + direction + '</span></h1>')
   var header = "<h2>You're about to erupt!!</h2>"
-  var body = '<img src="img/hot.png" alt="Hot">'
+  var body = '<img src="img/hot.gif" alt="Hot">'
   setModalWindow(header,body)
 }
 
