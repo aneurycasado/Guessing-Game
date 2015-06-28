1be0105f0273e81ab4d1cc10334c547d7148841f
Aneury Casado <aneury.casado@gmail.com>
2015-06-28 05:06:04 -0400

    Added comments. Live game can be found at aneurycasado.github.io/Guessing-Game

--- js/game.js
+++ js/game.js
@@ -1,57 +1,76 @@
-//This function waits until the document is ready
 $(function(){
-//This function picks my number
+//Pick a random number from 1-100
   var randomNumber = pickRandomNumber(1,100)
   var numberOfGuesses = 5
-//I use guessesSoFar to keep track of the guess and the temperature
-//associated with that guess
+//Store the guess and the temperature of the guess
   var guessesSoFar = [[],[]]
-//Previous difference keeps track of the last guess
-//It is used to update the judgment label
+//Keep track of the previous difference between guess and randomNumber
   var previousDifference = 0
-//Here, I use jquery to select 
   $("#submit").click(function(){
+//Lower the number of guesses and update it on the page
     numberOfGuesses-=1
     updateNumberOfGuesses(numberOfGuesses)
+//Get user input
     var guess = parseInt($("#guess").val())
+//Is the game over
     if(gameOver(numberOfGuesses,randomNumber,guess)){
+//If yes update the html page
       numberOfGuesses = updateGuessesSoFar(guess,'Loser',guessesSoFar)
       loser()
+//If no, then handle the input
     }else{
+//If the input is not valid, i.e not a number,
         if(isNaN(guess)){
           guessesSoFar = updateGuessesSoFar(guess,"IM",guessesSoFar)
+//Tell the user it is not valid. Decided to be a little cute with it.
           if(numberOfGuesses == 4){
-            okayFirstTime()
+            illegalMove("Hope you read the rules.")
           }else if(numberOfGuesses == 3){
-            comeOnMan()
+            illegalMove("Seriously, again.")
           }else if(numberOfGuesses == 2){
-            betterGetYourActTogether()
+            illegalMove("If you don't want to play, then just stop.")
           }else if(numberOfGuesses == 1){
-            yourGoingToLose()
+            illegalMove("Ladies and gentlemen, we have a loser in our midst.")
           }
+//The number is valid but is it a repeat?
+//I check if the number is a repeat by checking if the first array in guesses contains it
         }else if(guessesSoFar[0].indexOf(guess) > -1){
-          console.log("Here")
+//If the number is a repeat, then update the html page accordingly.
           guessesSoFar = updateGuessesSoFar(guess,'R',guessesSoFar)
           dontRepeatYourself()
+//Else the number is valid and not a repeat.
         }else{
           var difference = randomNumber - guess
           var direction = ""
+//If the guess is lower than the random number then
           if(difference > 0){
+//Notify the user to guess higher
             direction = "Higher"
+//Else, the guess is lower than the random number
           }else{
+//and they should guess lower
             direction = "Lower"
           }
+//Using absolute here to focus on the distance from the guess is from the
+//random number
           difference = Math.abs(difference)
+//Set previous difference for the first case.
           if(previousDifference == 0){
             previousDifference = difference
           }
+//After the first case, if the current difference is greater than the last difference
           if(difference > previousDifference){
+//The user is colder
             judgement('Colder')
           }else if(difference < previousDifference){
+//If the opposite is true than the user is hotter
             judgement('Hotter')
           }else if(difference == previousDifference){
+//Happens with repeat numbers and the first guess
             judgement("Same")
           }
+//Update the html page and guessesSoFar according to the absolute distance of
+//the random number from the guess
           if(difference > 40){
             guessesSoFar = updateGuessesSoFar(guess,'SC',guessesSoFar)
             superCold(direction)
@@ -69,63 +88,24 @@ $(function(){
           }else{
             thereIsNoHelpingYou()
           }
+//Set previousDifference
           previousDifference = difference
         }
       }
     });
+//Establish the hint button to provide the user with the answer
     $("#hint").click(function(){
       var header = "<h2>Well, just know you lost already but the answer is</h2>"
       var body = "<h1>" + randomNumber.toString() + "</h2>"
       setModalWindow(header,body)
     })
+//Enter key triggers submit button
     $(document).keypress(function(event){
       if(event.which == 13){
         $('#submit').trigger("click");
       }
     });
 })
-
-
-function judgement(string){
-  if(string == "Hotter"){
-    $("#judgement").html('<span class="label label-danger">' + string +'</span>')
-  }else if(string == "Colder"){
-    $("#judgement").html('<span class="label label-info">' + string +'</span>')
-  }else{
-    $("#judgement").html('<span class="label label-default">' + string +'</span>')
-  }
-}
-
-function updateGuessesSoFar(guess,temp,guessesSoFar){
-  guessesSoFar[0].push(guess)
-  guessesSoFar[1].push(temp)
-  updateGuessesSoFarHTML(guessesSoFar)
-  return guessesSoFar
-}
-
-function dontRepeatYourself(){
-  $("#talk").html("Come on. Don't repeat yourself.")
-  $("#indicator").html('<span class="label label-default">Repeat</span></h1>')
-  var header = "<h2>You already guessed that number.</h2>"
-  var body = '<img src="img/repeat.png" alt="Repeat">'
-  setModalWindow(header,body)
-}
-
-function updateGuessesSoFarHTML(guessesSoFar){
-  var string = createString(guessesSoFar)
-  $("#guessesSoFar").html('<span class="label label-info">' + string + '</span></h1>')
-}
-
-function createString(guessesSoFar){
-  var string = ""
-  for(var i = 0; i < guessesSoFar[0].length;i++){
-    var guess = guessesSoFar[0][i]
-    var temp = guessesSoFar[1][i]
-    string = string + guess.toString() + " " + temp + " | "
-  }
-  //string = substring(0,string.length-1)
-  return string
-}
 
 function pickRandomNumber(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -147,10 +127,27 @@ function gameOver(numberOfGuesses,randomNumber,guess){
   }
 }
 
-function illegalMove(){
-  var header = "<h2>Read this rule.</h2>"
-  var body = '<div class ="row"><div class="col-sm-12"><h2>Only numbers are valid inputs.</h2><b class = "glyphicon glyphicon-ok"></b></div></div>'
-  setModalWindow(header,body)
+function updateGuessesSoFar(guess,temp,guessesSoFar){
+  guessesSoFar[0].push(guess)
+  guessesSoFar[1].push(temp)
+  updateGuessesSoFarHTML(guessesSoFar)
+  return guessesSoFar
+}
+
+function updateGuessesSoFarHTML(guessesSoFar){
+  var string = createString(guessesSoFar)
+  $("#guessesSoFar").html('<span class="label label-info">' + string + '</span></h1>')
+}
+
+function createString(guessesSoFar){
+//Helper function that turns guessesSoFar into a string
+  var string = ""
+  for(var i = 0; i < guessesSoFar[0].length;i++){
+    var guess = guessesSoFar[0][i]
+    var temp = guessesSoFar[1][i]
+    string = string + guess.toString() + " " + temp + " | "
+  }
+  return string
 }
 
 function loser(){
@@ -162,28 +159,36 @@ function loser(){
   setModalWindow(header,body)
 }
 
-function okayFirstTime(){
-  $("#talk").html("I hope you read the rules this time.")
-  $("#indicator").html('<span class="label label-info">Ilegal Move</span></h1>')
-  illegalMove();
+function setModalWindow(header,body){
+  $(".modal-title").html(header)
+  $(".modal-body").html(body)
 }
 
-function comeOnMan(){
-  $("#talk").html("If you don't want to play, then just stop.")
+function illegalMove(string){
+  $("#talk").html(string)
   $("#indicator").html('<span class="label label-info">Ilegal Move</span></h1>')
-  illegalMove();
+  var header = "<h2>Read this rule.</h2>"
+  var body = '<div class ="row"><div class="col-sm-12"><h2>Only numbers are valid inputs.</h2><b class = "glyphicon glyphicon-ok"></b></div></div>'
+  setModalWindow(header,body)
 }
 
-function betterGetYourActTogether(){
-  $("#talk").html("Why am I even talking to you.")
-  $("#indicator").html('<span class="label label-info">Ilegal Move</span></h1>')
-  illegalMove();
+function dontRepeatYourself(){
+  $("#talk").html("Don't pick the same number.")
+  $("#indicator").html('<span class="label label-default">Repeat</span></h1>')
+  var header = "<h2>You already guessed that number.</h2>"
+  var body = '<img src="img/repeat.png" alt="Repeat">'
+  setModalWindow(header,body)
 }
 
-function yourGoingToLose(){
-  $("#talk").html("Ladies and gentlemen, we have a loser in our midst.")
-  $("#indicator").html('<span class="label label-info">Ilegal Move</span></h1>')
-  illegalMove();
+function judgement(string){
+//Changes the Judgment label on the html page
+  if(string == "Hotter"){
+    $("#judgement").html('<span class="label label-danger">' + string +'</span>')
+  }else if(string == "Colder"){
+    $("#judgement").html('<span class="label label-info">' + string +'</span>')
+  }else{
+    $("#judgement").html('<span class="label label-default">' + string +'</span>')
+  }
 }
 
 function superCold(direction){
@@ -230,9 +235,4 @@ function winner(){
 function thereIsNoHelpingYou(){
   $("#talk").html("I literally just can't even. Just guess again")
   $("#indicator").html('<span class="label label-default">Worst than Super Cold</span></h1>')
-}
-
-function setModalWindow(header,body){
-  $(".modal-title").html(header)
-  $(".modal-body").html(body)
 }
